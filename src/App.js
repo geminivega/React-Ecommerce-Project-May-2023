f66bd9d806a6f9b602399276245a7957160edfbc
Austin <github@sl.octavius.one>
2023-05-05 19:59:34 +0000

    added some cards. im also going to keep on typing more to try and go backs the 50 limit character limit thing just to see what happens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+
+      <p>this is my life now</p>
+      <Footer />
     </div>
   );
 }
