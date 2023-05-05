caff4c2b414d1889de97d637de28386dfdf47ebd
Austin <github@sl.octavius.one>
2023-05-05 20:24:46 +0000

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Navigation from "./Components/Navigation";
 
 export default function App() {
   return (
     <div className="App">
+      <Navigation />
       <Header />
-
       <p>this is my life now</p>
       <Footer />
     </div>
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <>
+    Navigation
+    <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+
+
+
+          <li>Link 4</li>
+    </ul>
+    </>
+  )
+}
+
+export default Navigation
