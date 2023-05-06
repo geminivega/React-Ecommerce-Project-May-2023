05ed273ac58859386ac4b325bac180b4fc153e80
Austin <github@sl.octavius.one>
2023-05-06 20:10:55 +0000

    Added slider

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Search } from '@mui/icons-material';
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
 
 const Container = styled.div`
   height: 60px;
@@ -37,9 +38,24 @@ const Input = styled.input`
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -49,11 +65,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input /> <Search />
+            <Input />
+            <Search style={{ color: 'gray', fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>AUSTIN</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
