471bd0871eccb4d23e313712dd68b9dc63f53c61
Austin <github@sl.octavius.one>
2023-05-06 19:17:56 +0000

    changed things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./styles.css";
-<<<<<<< HEAD
 import Home from "./Pages/Home";
-=======
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Navigation from "./Components/Navigation";
->>>>>>> caff4c2b414d1889de97d637de28386dfdf47ebd
 
 export default function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Home />
-=======
-      <Navigation />
-      <Header />
-      <p>this is my life now</p>
-      <Footer />
->>>>>>> caff4c2b414d1889de97d637de28386dfdf47ebd
     </div>
   );
 }
